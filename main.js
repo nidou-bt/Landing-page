6b6c4bae43bb6cd138e86751e038a17ea9b2602a
nidhal-bt <bentaaritn@gmail.com>
2023-02-01 11:42:40 +0100

    part 4

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
-  primaryNav.hasAttribute('data-visible') ? console.log("true") : console.log("it doesn't");
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
 });
